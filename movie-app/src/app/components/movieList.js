d04b1a9b8dcc1c133e65fd06cbf5777b00816b89
temka2222 <temuulen2222@gmail.com>
2025-03-24 21:08:45 -0700

    add

--- movie-app/src/app/components/movieList.js
+++ movie-app/src/app/components/movieList.js
@@ -1,15 +1,17 @@
 import { Star } from "./starLogo";
 export const MovieList = ({ url, name, rating }) => {
   return (
-    <div className="w-58 h-110 flex flex-col rounded-2">
-      <img className="h-340" src={url}></img>
-      <div className="flex flex-col h-24 bg-[#A1A1AA]">
-        <div className="flex flex-row justify-items-start items-center">
+    <div className="w-58 h-110 flex flex-col  ">
+      <img  className="w-full h-full rounded-t-2xl " src={url}></img>
+      <div className="flex flex-col h-24 bg-[#A1A1AA] rounded-b-2xl">
+        <div className="flex flex-row  pr-1/2 text-wrap">
           <Star />
+          
           <p>{rating}</p>
         </div>
-
+        <div className="w-34 text-wrap">
         <p>{name}</p>
+        </div>
       </div>
     </div>
   );
--- movie-app/src/app/components/popularlist.js
+++ movie-app/src/app/components/popularlist.js
@@ -1,15 +1,12 @@
 import { MovieList } from "./movieList";
 import { Seemore } from "./Seemore";
-export const UpcomingList = ({list}) => {
-  const newList=list.filter((item)=>{
-    return item.list=="upcoming"
-  })
+export const PopularList = () => {
   
   return (
     <div className="flex flex-col gap-8 p-20">
-      <Seemore title="Upcoming" />
+      <Seemore title="Popular" />
       <div className="grid grid-cols-5 gap-8  ">
-        {newList.map((item, index) => {  
+        {list.map((item, index) => {
           return (
             <div key={index}>
               <MovieList url={item.url} name={item.name} rating={item.rating} />

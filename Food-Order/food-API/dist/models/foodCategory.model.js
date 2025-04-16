b4fd2cd3b57af020b13add50e867081c9a450996
24LP8740 24LP8740 <24LP8740@administrators-iMac-2.local>
2025-04-16 16:01:46 +0800

    delete food order

--- Food-Order/food-API/dist/models/foodCategory.model.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=foodCategory.model.js.map
--- Food-Order/food-API/src/routes/food.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import express from "express";
-import { createFood } from "../controllers/Food";
-
-const foodRouter = express.Router();
-
-foodRouter.post("/", createFood);
-
-export default foodRouter;
